020d00bfbf96b2e4a0a0053ca2ebb15b60abd942
Exajd <exajd1989@gmail.com>
2019-12-07 14:12:39 +0000

    zmiana

--- Test.java
+++ Test.java
@@ -1,6 +1,6 @@
 public class Test {
     public static void main(String[] args) {
-        for (String s : "malpa" , "kot"), ("fs") {
+        for (String s : "malpa" , "kot"), ("fs"), ("fnesingis"){
             System.out.println (s);
             System.out.println (s);
             System.out.println (s);
